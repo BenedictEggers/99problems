ad5bdf4990bd8be5e7649e05f8f155995ccc95b2
Ben Eggers <beggerss@cs.washington.edu>
2014-07-13 20:37:53 -0700

    2 and 3

--- MyLists.hs
+++ MyLists.hs
@@ -5,13 +5,20 @@
 
 module MyLists
 ( myLast
+, myButLast
+, myElementAt
 , myLength
 ) where
 
 myLast [x] = x
 myLast (x:xs) = myLast xs
 
---myButLast []
+myButLast [x, y] = x
+myButLast (x:xs) = myButLast xs
+
+myElementAt xs n
+    | n == 1    = head xs
+    | otherwise = myElementAt (tail xs) (n - 1)
 
 myLength [] = 0
 myLength (x:xs) = 1 + myLength xs
--- tests.hs
+++ tests.hs
@@ -1,17 +1,24 @@
+-- Ben Eggers
+-- Tests for all my solutions
+
+ --TODO: Auto-populate test lists?
 
 module Tests where
 
-import MyLists( myLast, myLength )
+import MyLists( myLast, myButLast, myElementAt, myLength )
 import Test.HUnit
 
 main = runTestTT all_tests
 
 all_tests = TestList [test1thru10]
 
-test1thru10 = TestList [testMyLast1, testMyLast2, testMyLast3
-                        , testMyLength1, testMyLength2, testMyLength3]
+test1thru10 = TestList [testMyLast
+                      , testMyButLast
+                      , testMyElementAt
+                      , testMyLength]
 
 -- Problem 1
+testMyLast = TestList [testMyLast1, testMyLast2, testMyLast3]
 testMyLast1 = TestCase (assertEqual "3 should be the last element of [1,2,3]"
                         3 (myLast [1,2,3]))
 testMyLast2 = TestCase (assertEqual "4 should be the last element of [1,2,3,4]"
@@ -19,7 +26,29 @@ testMyLast2 = TestCase (assertEqual "4 should be the last element of [1,2,3,4]"
 testMyLast3 = TestCase (assertEqual "'z' should be the last element of ['x','y','z']"
                         'z' (myLast ['x','y','z']))
 
+-- Problem 2
+testMyButLast = TestList [testMyButLast1, testMyButLast2, testMyButLast3]
+testMyButLast1 = TestCase (assertEqual "2 should be the 2nd-to-last of [1,2,3]"
+                            2 (myButLast [1,2,3]))
+testMyButLast2 = TestCase (assertEqual "3 should be the 2nd-to-last of [1,2,3,4]"
+                            3 (myButLast [1,2,3,4]))
+testMyButLast3 = TestCase (assertEqual "'y' should be the 2nd-to-last of ['a'..'z']"
+                            'y' (myButLast ['a'..'z']))
+
+-- Problem 3
+testMyElementAt = TestList [testMyElementAt1, testMyElementAt2
+                          , testMyElementAt3, testMyElementAt4]
+testMyElementAt1 = TestCase (assertEqual "1 should be the first element of [1,2,3]"
+                              1 (myElementAt [1,2,3] 1))
+testMyElementAt2 = TestCase (assertEqual "2 should be the second element of [1,2,3]"
+                              2 (myElementAt [1,2,3] 2))
+testMyElementAt3 = TestCase (assertEqual "3 should be the third element of [1,2,3]"
+                              3 (myElementAt [1,2,3] 3))
+testMyElementAt4 = TestCase (assertEqual "'j' should be the 10th element of ['a'..'z']"
+                              'j' (myElementAt ['a'..'z'] 10))
+
 -- Problem 4
+testMyLength = TestList [testMyLength1, testMyLength2, testMyLength3]
 testMyLength1 = TestCase (assertEqual "Length of [] should be 0"
                           0 (myLength []))
 testMyLength2 = TestCase (assertEqual "Length of [1,2,3] should be 3"

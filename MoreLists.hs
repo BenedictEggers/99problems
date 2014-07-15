d7b509735fce26b3aabcd90613bbb0b671f4991e
Ben Eggers <beggerss@cs.washington.edu>
2014-07-14 22:28:33 -0700

    11-20 stubs

---
+++ MoreLists.hs
@@ -0,0 +1,34 @@
+-- Ben Eggers <ben.eggers36@gmail.com>
+-- Problems 11-20
+
+module MoreLists where
+
+-- Problem 11
+encodeModified xs = []
+
+-- Proble 12
+decodeModified xs = []
+
+-- Problem 13
+encodeDirect xs = []
+
+-- Problem 14
+dupli xs = []
+
+-- Problem 15
+repli xs n= []
+
+-- Problem 16
+dropEvery xs n = []
+
+-- Problem 17
+split xs n = []
+
+-- Problem 18
+slice list start end = []
+
+-- Problem 19
+rotate xs n = []
+
+-- Problem 20
+removeAt n xs = []
